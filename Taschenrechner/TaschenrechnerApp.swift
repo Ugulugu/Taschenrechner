48f93ac01c9cc64458d269cc71be299495384ab7
Max <maximilian@dahos.de>
2021-07-14 17:25:04 +0200

    Initial Commit

---
+++ Taschenrechner/TaschenrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaschenrechnerApp.swift
+//  Taschenrechner
+//
+//  Created by Maximilian Schröder on 14.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TaschenrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
